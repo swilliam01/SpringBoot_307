76943ee08d6c76e8f13c588f66805babfa5de7f6
swilliam01 <sehrishwilliam@gmail.com>
2018-10-30 22:57:00 -0400

    i have it

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @RequestMapping("/")
+    public String index(Model model){
+        Employee employee = new Employee();
+        employee.setName("Stephen Bullock");
+        employee.setSsn("555-12-1234");
+
+        Laptop laptop = new Laptop();
+        laptop.setBrand("Dell");
+        laptop.setModel("Latitude XL");
+
+        employee.setLaptop(laptop);
+        employeeRepository.save(employee);
+        model.addAttribute("employee", employeeRepository.findAll());
+        return "index";
+    }
+
+}
+
